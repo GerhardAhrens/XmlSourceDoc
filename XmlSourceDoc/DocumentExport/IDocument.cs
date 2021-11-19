8ba44924357213e4556ee9f5076088f227550026
Gerhard Ahrens <Gerhard.Ahrens@pta.de>
2021-11-19 08:39:25 +0100

    *

---
+++ XmlSourceDoc/DocumentExport/IDocument.cs
@@ -0,0 +1,15 @@
+﻿namespace XmlSourceDoc.DocumentExport
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IDocument
+    {
+        string Assembly { get; }
+
+        string Namespace { get; }
+    }
+}
